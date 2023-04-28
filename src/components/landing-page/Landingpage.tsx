939323498584c4f6e6d89e0b15cecf5419702c46
Oskar Magnefelt <magnefelt.oskar@gmail.com>
2023-04-28 13:54:11 +0200

    lagt till import och användning av google fonts

--- src/components/landing-page/Landingpage.tsx
+++ src/components/landing-page/Landingpage.tsx
@@ -1,12 +1,14 @@
-import React from 'react'
-import Herolandingpage from './Herolandingpage'
+import React from "react";
+import Herolandingpage from "./Herolandingpage";
+import ProductPage from "../product-page/ProductPage";
 
 const Landingpage = () => {
   return (
     <div>
       <Herolandingpage></Herolandingpage>
+      {/* <ProductPage></ProductPage> */}
     </div>
-  )
-}
+  );
+};
 
-export default Landingpage
+export default Landingpage;
