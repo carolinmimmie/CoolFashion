fc994f7eaf7435bdb772b96fefadaaf8e41e1ea7
Mili01 <miligolic2022@gmail.com>
2023-05-13 14:22:28 +0200

    Skapat Search Form - Söker på Title & Price

--- src/components/pages/search-page/SearchPage.tsx
+++ src/components/pages/search-page/SearchPage.tsx
@@ -1,52 +1,81 @@
-import React from 'react';
+import React, { useState, useEffect } from "react";
+import { Table, Form } from "react-bootstrap";
+import { getAllProducts } from "../../../Api";
+import { IAllProducts } from "../../../interfaces";
 
 const SearchPage = () => {
+  const [productList, setProductList] = useState<IAllProducts[]>([]);
+  const [searchTerm, setSearchTerm] = useState("");
+  const [searchResults, setSearchResults] = useState<IAllProducts[]>([]);
+  const [showResults, setShowResults] = useState(false);
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      const products = await getAllProducts();
+      setProductList(products);
+    };
+    fetchProducts();
+  }, []);
+
+  useEffect(() => {
+    const results = productList.filter((product) =>
+      product.title.toLowerCase().includes(searchTerm.toLowerCase()) ||
+      product.price.toString().includes(searchTerm)
+    );
+    setSearchResults(results);
+    setShowResults(searchTerm !== "");
+  }, [searchTerm, productList]);
+
+  const handleSearchChange = (event: { target: { value: React.SetStateAction<string>; }; }) => {
+    setSearchTerm(event.target.value);
+  };
+
   return (
-    <div className='container_on_search_page'>
-    <table className="table table-hover">
-      <thead>
-        <tr className="gray-bg text-white">
-          <th scope="col">#</th>
-          <th scope="col">Product Picture</th>
-          <th scope="col">Title</th>
-          <th scope="col">Text</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr>
-          <th scope="row">1</th>
-          <td>Mark</td>
-          <td>Otto</td>
-          <td>@mdo</td>
-        </tr>
-        <tr>
-          <th scope="row">2</th>
-          <td>Jacob</td>
-          <td>Thornton</td>
-          <td>@fat</td>
-        </tr>
-        <tr>
-          <th scope="row">3</th>
-          <td>Jacob</td>
-          <td>Thornton</td>
-          <td>@fat</td>
-        </tr>
-        <tr>
-          <th scope="row">4</th>
-          <td>Jacob</td>
-          <td>Thornton</td>
-          <td>@fat</td>
-        </tr>
-        <tr>
-          <th scope="row">5</th>
-          <td>Jacob</td>
-          <td>Thornton</td>
-          <td>@fat</td>
-        </tr>
-      </tbody>
-    </table>
+    <div className="search_wrapper">
+    <div className="container_search">
+      <h3 className="search_products">Search Products</h3>
+      <Form>
+        <Form.Group controlId="searchForm">
+          <Form.Control
+          className="search_field"
+            type="text"
+            placeholder="Search by title or price..."
+            value={searchTerm}
+            onChange={handleSearchChange}
+            style={{ width: "50%" }} // Set the width to 50%
+          />
+        </Form.Group>
+      </Form>
+      {showResults && (
+        <Table striped bordered hover>
+          <thead>
+            <tr>
+              <th>Picture</th>
+              <th>Title</th>
+              <th>Price</th>
+            </tr>
+          </thead>
+          <tbody>
+            {searchResults.map((product) => (
+              <tr key={product.id}>
+                <td>
+                  <img
+                    src={product.image}
+                    alt={product.title}
+                    width="30"
+                    height="30"
+                  />
+                </td>
+                <td>{product.title}</td>
+                <td>{product.price} kr</td>
+              </tr>
+            ))}
+          </tbody>
+        </Table>
+      )}
+    </div>
     </div>
   );
-}
+};
 
 export default SearchPage;
