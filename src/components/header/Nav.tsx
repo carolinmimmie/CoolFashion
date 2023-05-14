866d72e0be31052276c2535b55eedba3e9589a82
Mili01 <miligolic2022@gmail.com>
2023-05-14 18:35:57 +0200

    Fixat så att det går söka via Navbarens SökFält

--- src/components/header/Nav.tsx
+++ src/components/header/Nav.tsx
@@ -15,26 +15,22 @@ import {
   Toolbar,
   Typography,
 } from "@mui/material";
-import TextField from "@mui/material/TextField";
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
 import MenuIcon from "@mui/icons-material/Menu";
 import SearchIcon from "@mui/icons-material/Search";
 import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
 import PersonOutlineIcon from "@mui/icons-material/PersonOutline";
 import WorkOutlineIcon from "@mui/icons-material/WorkOutline";
 import BasicMenu from "./BasicMenu";
-import { Link } from "react-router-dom";
+import { getAllProducts } from "../../Api";
+import { IAllProducts } from "../../interfaces";
 
-// import { Link, useNavigate } from "react-router-dom";
+
 interface Props {
-  /**
-   * Injected by the documentation to work in an iframe.
-   * You won't need it on your project.
-   */
   window?: () => Window;
 }
-const drawerWidth = 240;
-const navItems = ["Produkter", "Contact", "Admin"];
+
 const Search = styled("div")(({ theme }) => ({
   position: "relative",
   borderRadius: theme.shape.borderRadius,
@@ -50,6 +46,9 @@ const Search = styled("div")(({ theme }) => ({
   },
 }));
 
+
+const drawerWidth = 240;
+
 const SearchIconWrapper = styled("div")(({ theme }) => ({
   padding: theme.spacing(0, 2),
   height: "100%",
@@ -64,7 +63,6 @@ const StyledInputBase = styled(InputBase)(({ theme }) => ({
   color: "inherit",
   "& .MuiInputBase-input": {
     padding: theme.spacing(1, 1, 1, 0),
-    // vertical padding + font size from searchIcon
     paddingLeft: `calc(1em + ${theme.spacing(4)})`,
     transition: theme.transitions.create("width"),
     width: "100%",
@@ -79,7 +77,33 @@ const StyledInputBase = styled(InputBase)(({ theme }) => ({
 
 const Nav = (props: Props) => {
   const { window } = props;
-  const [mobileOpen, setMobileOpen] = React.useState(false);
+  const [mobileOpen, setMobileOpen] = useState(false);
+  const [productList, setProductList] = useState<IAllProducts[]>([]);
+  const [searchTerm, setSearchTerm] = useState("");
+  const [searchResults, setSearchResults] = useState<IAllProducts[]>([]);
+  const [showResults, setShowResults] = useState(false);
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      const products = await getAllProducts();
+      setProductList(products);
+    };
+    fetchProducts();
+  }, []);
+
+  useEffect(() => {
+    const results = productList.filter(
+      (product) =>
+        product.title.toLowerCase().includes(searchTerm.toLowerCase()) ||
+        product.price.toString().includes(searchTerm)
+    );
+    setSearchResults(results);
+    setShowResults(searchTerm !== "");
+  }, [searchTerm, productList]);
+
+  const handleSearchChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchTerm(event.target.value);
+  };
 
   const handleDrawerToggle = () => {
     setMobileOpen((prevState) => !prevState);
@@ -88,163 +112,178 @@ const Nav = (props: Props) => {
   const drawer = (
     <div>
       <Box onClick={handleDrawerToggle} sx={{ textAlign: "center" }}>
-        <Link className="hamburger-link-div" to={"/"}>
-          <ListItemText
-            primary={
-              <Typography variant="h6" sx={{ my: 2 }}>
-                COOL FASHION
-              </Typography>
-            }
-          />
+<Link className="hamburger-link-div" to={"/"}>
+<ListItemText
+primary={
+<Typography variant="h6" sx={{ my: 2 }}>
+COOL FASHION
+</Typography>
+}
+/>
+</Link>
+
+<Divider />
+    <List sx={{ ml: 2 }}>
+      <ListItem disablePadding>
+        <ListItemText primary={`Produkter`} />
+        <BasicMenu></BasicMenu>
+      </ListItem>
+
+      <ListItem disablePadding>
+        <Link className="hamburger-link-div" to={"contact"}>
+          <ListItemText primary={`Kontakta Oss`} />
         </Link>
+      </ListItem>
 
-        <Divider />
-        <List sx={{ ml: 2 }}>
-          {/* {navItems.map((item) => (
-            <ListItem key={item} disablePadding>
-              <ListItemText primary={item} />
-            </ListItem>
-          ))} */}
+      <ListItem disablePadding>
+        <Link className="hamburger-link-div" to={"admin"}>
+          <ListItemText primary={`Admin`} />
+        </Link>
+      </ListItem>
+    </List>
+  </Box>
+</div>
+);
 
-          <ListItem disablePadding>
-            <ListItemText primary={`Produkter`} />
-            <BasicMenu></BasicMenu>
-          </ListItem>
-
-          <ListItem disablePadding>
-            <Link className="hamburger-link-div" to={"contact"}>
-              <ListItemText primary={`Kontakta Oss`} />
-            </Link>
-          </ListItem>
-
-          <ListItem disablePadding>
-            <Link className="hamburger-link-div" to={"admin"}>
-              <ListItemText primary={`Admin`} />
-            </Link>
-          </ListItem>
-        </List>
-      </Box>
-    </div>
-  );
-
-  const container =
+    const container =
     window !== undefined ? () => window().document.body : undefined;
-  return (
+    return (
     <>
-      <Box>
-        <AppBar component="nav" sx={{ bgcolor: "black" }}>
-          <Toolbar className="nav-container">
-            <div>
-              <IconButton
-                color="inherit"
-                aria-label="open drawer"
-                edge="start"
-                onClick={handleDrawerToggle}
-                sx={{
-                  mr: 2,
-                  display: { sm: "block", md: "none" },
-                  flexGrow: 0,
-                }}
-              >
-                <MenuIcon />
-              </IconButton>
-              <Box className="nav-container-links-and-h1">
-                <Typography
-                  variant="h1"
-                  component="div"
-                  sx={{
-                    flexGrow: 0.05,
-                    display: { xs: "block", sm: "block" },
-                    fontSize: "20px",
-                    color: "white",
-                  }}
-                >
-                  {
-                    <Link className="logga" to={"/"}>
-                      COOL FASHION
-                    </Link>
-                  }
-                </Typography>
+    <Box>
+    <AppBar component="nav" sx={{ bgcolor: "black" }}>
+    <Toolbar className="nav-container">
+    <div>
+    <IconButton
+    color="inherit"
+    aria-label="open drawer"
+    edge="start"
+    onClick={handleDrawerToggle}
+    sx={{
+    mr: 2,
+    display: { sm: "block", md: "none" },
+    flexGrow: 0,
+    }}
+    >
+    <MenuIcon />
+    </IconButton>
+    <Box className="nav-container-links-and-h1">
+    <Typography
+    variant="h1"
+    component="div"
+    sx={{
+    flexGrow: 0.05,
+    display: { xs: "block", sm: "block" },
+    fontSize: "20px",
+    color: "white",
+    }}
+    >
+    <Link className="logga" to={"/"}>
+    COOL FASHION
+    </Link>
+    </Typography>
+            <Link to={"/"}>
+              <BasicMenu></BasicMenu>
+            </Link>
 
-                <Link to={"/"}>
-                  <BasicMenu></BasicMenu>
-                </Link>
-
-                <Link to={"contact"} className="link-div">
-                  Kontakta Oss
-                </Link>
-                <Link to={"admin"} className="link-div">
-                  Admin
-                </Link>
-                {/* <Box
-                  sx={{
-                    display: { xs: "none", sm: "none", md: "block" },
-                    flexGrow: { sm: 1 },
-                  }}
-                ></Box> */}
-              </Box>
-            </div>
-
-            <Box className="boxserchandicon" sx={{}}>
-              <Search sx={{ display: { xs: "none", sm: "none", md: "block" } }}>
-                <SearchIconWrapper>
-                  <SearchIcon />
-                </SearchIconWrapper>
-                <StyledInputBase
-                  placeholder="Sök produkter"
-                  inputProps={{ "aria-label": "search" }}
-                  className="serchfield"
-                />
-              </Search>
-              <Box sx={{ margin: "auto" }}>
-                <FavoriteBorderIcon sx={{ ml: 2 }} />
-                <PersonOutlineIcon sx={{ ml: 2 }} />
-                <WorkOutlineIcon sx={{ ml: 2 }} />
-              </Box>
-            </Box>
-          </Toolbar>
-          <Box
-            className="serchfieldxsscreen"
-            sx={{ display: { xs: "block", sm: "block", md: "none" } }}
-          >
-            <Search sx={{}}>
-              <SearchIconWrapper>
-                <SearchIcon />
-              </SearchIconWrapper>
-              <StyledInputBase
-                placeholder="Sök produkter"
-                inputProps={{ "aria-label": "search" }}
-                className="serchfield"
-                //Här under är det som behöver skickas till Milis komponent
-                // value={searchTerm}
-                // onChange={handleSearchChange}
-              />
-            </Search>
+            <Link to={"contact"} className="link-div">
+            Kontakta Oss
+            </Link>
+            <Link to={"admin"} className="link-div">
+              Admin
+            </Link>
           </Box>
-        </AppBar>
-        <Box component="nav">
-          <Drawer
-            container={container}
-            variant="temporary"
-            open={mobileOpen}
-            onClose={handleDrawerToggle}
-            ModalProps={{
-              keepMounted: true, // Better open performance on mobile.
-            }}
-            sx={{
-              display: { xs: "block", sm: "block", md: "none" },
-              "& .MuiDrawer-paper": {
-                boxSizing: "border-box",
-                width: drawerWidth,
-              },
-            }}
-          >
-            {drawer}
-          </Drawer>
+        </div>
+
+        <Box className="boxserchandicon" sx={{}}>
+          <Search sx={{ display: { xs: "none", sm: "none", md: "block" } }}>
+            <SearchIconWrapper>
+              <SearchIcon />
+            </SearchIconWrapper>
+            <StyledInputBase
+              placeholder="Sök produkter"
+              inputProps={{ "aria-label": "search" }}
+              className="serchfield"
+              value={searchTerm}
+              onChange={handleSearchChange}
+            />
+          </Search>
+          <Box sx={{ margin: "auto" }}>
+            <FavoriteBorderIcon sx={{ ml: 2 }} />
+            <PersonOutlineIcon sx={{ ml: 2 }} />
+            <WorkOutlineIcon sx={{ ml: 2 }} />
+          </Box>
         </Box>
+      </Toolbar>
+      <Box
+        className="serchfieldxsscreen"
+        sx={{ display: { xs: "block", sm: "block", md: "none" } }}
+      >
+        <Search sx={{}}>
+          <SearchIconWrapper>
+            <SearchIcon />
+          </SearchIconWrapper>
+          <StyledInputBase
+            placeholder="Sök produkter"
+            inputProps={{ "aria-label": "search" }}
+            className="serchfield"
+            value={searchTerm}
+            onChange={handleSearchChange}
+          />
+        </Search>
       </Box>
-    </>
-  );
+    </AppBar>
+    <Box component="nav">
+      <Drawer
+        container={container}
+        variant="temporary"
+        open={mobileOpen}
+        onClose={handleDrawerToggle}
+        ModalProps={{
+        keepMounted: true,
+        }}
+        sx={{
+        display: { xs: "block", sm: "block", md: "none" },
+        "& .MuiDrawer-paper": {
+        boxSizing: "border-box",
+        width: drawerWidth,
+        },
+        }}
+        >
+        {drawer}
+        </Drawer>
+        </Box>
+        </Box>
+  {showResults && (
+    <div className="search_results">
+      <table className="table table-striped table-bordered table-hover">
+        <thead>
+          <tr>
+            <th>Picture</th>
+            <th>Title</th>
+            <th>Price</th>
+          </tr>
+        </thead>
+        <tbody>
+          {searchResults.map((product) => (
+            <tr key={product.id}>
+              <td>
+                <img
+                  src={product.image}
+                  alt={product.title}
+                  width="30"
+                  height="30"
+                />
+              </td>
+              <td>{product.title}</td>
+              <td>{product.price} kr</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  )}
+</>
+);
 };
 
 export default Nav;
