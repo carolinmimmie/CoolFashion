65df0a610d84fbc72cf5a02477aaddda91aaee18
Carolin Andersson <carolinmimmie@gmail.com>
2023-05-15 21:36:46 +0200

    PopUp Cart i nav när man klicka på varukorgen

--- src/components/header/Nav.tsx
+++ src/components/header/Nav.tsx
@@ -1,16 +1,8 @@
 import {
   alpha,
   AppBar,
-  Box,
-  Button,
-  Divider,
-  Drawer,
   IconButton,
   InputBase,
-  List,
-  ListItem,
-  ListItemButton,
-  ListItemText,
   styled,
   Toolbar,
   Typography,
@@ -27,6 +19,19 @@ import { getAllProducts } from "../../Api";
 import { IAllProducts } from "../../interfaces";
 import Context from "../../context/Context";
 import Cart from "./Cart";
+import Box from "@mui/material/Box";
+import Drawer from "@mui/material/Drawer";
+import Button from "@mui/material/Button";
+import List from "@mui/material/List";
+import Divider from "@mui/material/Divider";
+import ListItem from "@mui/material/ListItem";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import ListItemText from "@mui/material/ListItemText";
+import InboxIcon from "@mui/icons-material/MoveToInbox";
+import MailIcon from "@mui/icons-material/Mail";
+
+type Anchor = "right";
 
 interface Props {
   window?: () => Window;
@@ -76,6 +81,60 @@ const StyledInputBase = styled(InputBase)(({ theme }) => ({
 }));
 
 const Nav = (props: Props) => {
+  const [state, setState] = React.useState({
+    right: false,
+  });
+
+  const toggleDrawer =
+    (anchor: Anchor, open: boolean) =>
+    (event: React.KeyboardEvent | React.MouseEvent) => {
+      if (
+        event.type === "keydown" &&
+        ((event as React.KeyboardEvent).key === "Tab" ||
+          (event as React.KeyboardEvent).key === "Shift")
+      ) {
+        return;
+      }
+
+      setState({ ...state, [anchor]: open });
+    };
+
+  const list = (anchor: Anchor) => (
+    <Box
+      //   sx={{ width: anchor === 'top' || anchor === 'bottom' ? 'auto' : 250 }}
+      role="presentation"
+      onClick={toggleDrawer(anchor, false)}
+      onKeyDown={toggleDrawer(anchor, false)}
+    >
+      <List>
+        {["", "Starred", "Send email", "Drafts"].map((text, index) => (
+          <ListItem key={text} disablePadding>
+            <ListItemButton>
+              <ListItemIcon>
+                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+              </ListItemIcon>
+              <ListItemText primary={text} />
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
+      <Divider />
+      <List>
+        {["All mail", "Trash", "Spam"].map((text, index) => (
+          <ListItem key={text} disablePadding>
+            <ListItemButton>
+              <ListItemIcon>
+                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+              </ListItemIcon>
+              <ListItemText primary={text} />
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
+    </Box>
+  );
+  //Slut Cart
+
   const { window } = props;
   const [mobileOpen, setMobileOpen] = useState(false);
 
@@ -130,6 +189,23 @@ const Nav = (props: Props) => {
     window !== undefined ? () => window().document.body : undefined;
   return (
     <>
+      {/* <div>
+      {(["right"] as const).map((anchor) => (
+        <React.Fragment key={anchor}>
+          <WorkOutlineIcon onClick={toggleDrawer(anchor, true)}>
+            {anchor}
+          </WorkOutlineIcon>
+          <Drawer
+            anchor={anchor}
+            open={state[anchor]}
+            onClose={toggleDrawer(anchor, false)}
+          >
+            {list(anchor)}
+          </Drawer>
+        </React.Fragment>
+      ))}
+    </div> */}
+
       <Box>
         <AppBar component="nav" sx={{ bgcolor: "black" }}>
           <Toolbar className="nav-container">
@@ -193,8 +269,24 @@ const Nav = (props: Props) => {
                   <FavoriteBorderIcon sx={{ ml: 2 }} />
                 </Link>
                 <PersonOutlineIcon sx={{ ml: 2 }} />
-
-                <WorkOutlineIcon sx={{ ml: 2 }} />
+                {(["right"] as const).map((anchor) => (
+                  <React.Fragment key={anchor}>
+                    <WorkOutlineIcon
+                      className="icon-link"
+                      sx={{ ml: 2 }}
+                      onClick={toggleDrawer(anchor, true)}
+                    >
+                      {anchor}
+                    </WorkOutlineIcon>
+                    <Drawer
+                      anchor={anchor}
+                      open={state[anchor]}
+                      onClose={toggleDrawer(anchor, false)}
+                    >
+                      {list(anchor)}
+                    </Drawer>
+                  </React.Fragment>
+                ))}
                 <div
                   className="rounded-circle bg-danger d-flex justify-content-center align-items-center"
                   style={{
